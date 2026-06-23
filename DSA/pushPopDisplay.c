244af930d05154361f1b3acca181a9190b7beb10
Dhruthidhar <dhruthidhar2007@gmail.com>
2026-06-23 09:06:26 +0530

    Updated push pop program

---
+++ DSA/pushPopDisplay.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+int stack[100];
+int top = -1;
+int size;
+
+void push(int value)
+{
+    if (top == size - 1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        top++;
+        stack[top] = value;
+        printf("%d pushed into stack\n", value);
+    }
+}
+
+void pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Stack elements are:\n");
+
+        for (int i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main()
+{
+    int choice, value;
+
+    printf("Enter size of stack: ");
+    scanf("%d", &size);
+
+    while (1)
+    {
+        printf("\n1. Push");
+        printf("\n2. Pop");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
+}
